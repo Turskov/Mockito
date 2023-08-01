6b070c4adc6ecfd28cf57b27cc1af6af570ccb91
tsn <turskov.sn@yandex.ru>
2023-08-01 23:00:16 +0300

    New tests

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -2,9 +2,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.services.MovieManager;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class MovieManagerTest {
     @Test
     public void shouldFindAllMoviesWhenBelowLimit() {
@@ -14,7 +11,8 @@ public class MovieManagerTest {
         movieManager.save("Отель Белград");
 
         String[] actualMovies = movieManager.findAll();
-        Assertions.assertEquals(3, actualMovies.length);
+        String[] expectedAllMovies = {"Бладшот", "Вперед", "Отель Белград"};
+        Assertions.assertArrayEquals(expectedAllMovies, actualMovies);
     }
 
     @Test
@@ -31,7 +29,7 @@ public class MovieManagerTest {
 
     @Test
     public void shouldSetLimitToFiveByDefault() {
-        MovieManager movieManager = new MovieManager();
+        MovieManager movieManager = new MovieManager(5);
 
         movieManager.save("Бладшот");
         movieManager.save("Вперед");
@@ -41,7 +39,8 @@ public class MovieManagerTest {
         movieManager.save("Тролли. Мировой тур");
 
         String[] actualMovies = movieManager.findAll();
+        String[] expectedMovies = {"Вперед", "Отель Белград", "Джентльмены", "Человек-невидимка", "Тролли. Мировой тур"};
 
-        Assertions.assertEquals(5, actualMovies.length);
+        Assertions.assertArrayEquals(expectedMovies, actualMovies);
     }
 }
